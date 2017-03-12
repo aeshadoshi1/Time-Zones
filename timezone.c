bb9a4004308a00196beaf078bfed5d79d2eaefcc
aeshadoshi1 <aeshadoshi1@gmail.com>
2017-03-12 15:16:00 -0400

    Update timezone.c
    "Hawaii, US" to "Honolulu, US" 
    City printed from "printf" statement instead of being stored in buffer

--- timezone.c
+++ timezone.c
@@ -12,25 +12,25 @@ int main (void) {
 	setenv("TZ", "US/Hawaii", 1);
 	tzset();
 	t1 = localtime(&t0);
-	strftime(buffer, 100, "Hawaii, US: %I:%M:%S %p", t1);
-	printf("%s\n", buffer);
+	strftime(buffer, 100, "%I:%M:%S %p", t1);
+	printf("Honolulu, US: %s\n", buffer);
 	
 	setenv("TZ", "America/Sao_Paulo", 1);
 	tzset();
 	t2 = localtime(&t0);
-	strftime(buffer, 100, "Sao Paulo, Brazil: %I:%M:%S %p", t2);
-	printf("%s\n", buffer);
+	strftime(buffer, 100, "%I:%M:%S %p", t2);
+	printf("Sao Paulo, Brazil: %s\n", buffer);
 	
 	setenv("TZ", "Europe/London", 1);
 	tzset();
 	t3 = localtime(&t0);
-	strftime(buffer, 100, "London, England: %I:%M:%S %p", t3);
-	printf("%s\n", buffer);
+	strftime(buffer, 100, "%I:%M:%S %p", t3);
+	printf("London, England: %s\n", buffer);
 	
 	setenv("TZ", "Asia/Tokyo", 1);
 	tzset();
 	t4 = localtime(&t0);
-	strftime(buffer, 100, "Tokyo, Japan: %I:%M:%S %p", t4);
-	printf("%s\n", buffer);
+	strftime(buffer, 100, "%I:%M:%S %p", t4);
+	printf("Tokyo, Japan: %s\n", buffer);
 	
 }
