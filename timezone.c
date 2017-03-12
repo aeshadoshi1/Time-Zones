6d7d9d564f93d83e0846b70946c942058616961a
aeshadoshi1 <aeshadoshi1@gmail.com>
2017-03-12 15:10:24 -0400

    Add files via upload
    Displays time from four cities (Honolulu, Sao Paulo, London, Tokyo)

---
+++ timezone.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main (void) {
+
+	char buffer[100];
+	time_t t0 = time(NULL);
+	struct tm *t1, *t2, *t3, *t4;
+	
+	setenv("TZ", "US/Hawaii", 1);
+	tzset();
+	t1 = localtime(&t0);
+	strftime(buffer, 100, "Hawaii, US: %I:%M:%S %p", t1);
+	printf("%s\n", buffer);
+	
+	setenv("TZ", "America/Sao_Paulo", 1);
+	tzset();
+	t2 = localtime(&t0);
+	strftime(buffer, 100, "Sao Paulo, Brazil: %I:%M:%S %p", t2);
+	printf("%s\n", buffer);
+	
+	setenv("TZ", "Europe/London", 1);
+	tzset();
+	t3 = localtime(&t0);
+	strftime(buffer, 100, "London, England: %I:%M:%S %p", t3);
+	printf("%s\n", buffer);
+	
+	setenv("TZ", "Asia/Tokyo", 1);
+	tzset();
+	t4 = localtime(&t0);
+	strftime(buffer, 100, "Tokyo, Japan: %I:%M:%S %p", t4);
+	printf("%s\n", buffer);
+	
+}
